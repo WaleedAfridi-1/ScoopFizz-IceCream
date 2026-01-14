6b2875861bde015167e9b901a9cb89e88a355883
Waleed Afridi <patlu7150@gmail.com>
2026-01-14 21:26:02 +0500

    Update vite.config.js

--- ScoopFizz/vite.config.js
+++ ScoopFizz/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './',
   root: resolve(__dirname, 'src'),
   build: {
     outDir: resolve(__dirname, 'dist'),
