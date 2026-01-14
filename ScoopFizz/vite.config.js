0e72b8268835b6a2b4a82f4aa818fd3a6d2d419c
Waleed Afridi <patlu7150@gmail.com>
2026-01-14 21:21:42 +0500

    Update vite.config.js

--- ScoopFizz/vite.config.js
+++ ScoopFizz/vite.config.js
@@ -5,6 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   base: '/ScoopFizz-IceCream/'
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: resolve(__dirname, 'src/index.html')
+    }
+  }
 
 })
