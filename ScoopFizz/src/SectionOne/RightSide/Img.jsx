c7a95dac4da3ebdcad54e69092701baa4e47534a
Waleedkhani255 <201253134+WaleedAfridi-1@users.noreply.github.com>
2026-01-14 18:35:10 +0500

    configure for github pages

--- ScoopFizz/src/SectionOne/RightSide/Img.jsx
+++ ScoopFizz/src/SectionOne/RightSide/Img.jsx
@@ -3,9 +3,9 @@ import {easeInOut, motion} from "framer-motion"
 const Img = () => {
   return (
     <motion.div 
-      initial={{x:40, opacity:0}}
+      initial={{x:40, opacity:0,rotate:0}}
       animate={{opacity:1, x:0, rotate:0, scale:1}}
-      whileHover={{rotate:[4,-4,4], transition:{duration:5.5, repeat:Infinity}}}
+      whileHover={{rotate:[0,-4,4,0], transition:{duration:4, repeat:Infinity,ease:'linear'}}}
       transition={{duration:0.6, ease:easeInOut}}
         whileTap={{scale:0.90, transition:{duration:0.2}}}
     
